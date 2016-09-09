2844d8ca67665bc2defa2e6842f2c1f5a76059d6
Joe Blacky <gooddealthanks@gmail.com>
2016-09-09 13:15:33 +0300

    IE flex-box bug fixing.

--- wp-content/themes/lipovskiy/common/forms/contact-form.php
+++ wp-content/themes/lipovskiy/common/forms/contact-form.php
@@ -1,23 +1,29 @@
 <form id="contact-form" action="<?php echo get_template_directory_uri() . '/ajax/forms.php' ?>" method="POST">
-  <fieldset class="flex fields">
+  <div class="flex fields">
     <fieldset class="field">
-      <label for="contact-name">
-        <?php _e('Name'); ?>
-      </label>
-      <input class="required" type="text" id="contact-name" name="name"/>
+      <div class="field-inner">
+        <label for="contact-name">
+          <?php _e('Name'); ?>
+        </label>
+        <input class="required" type="text" id="contact-name" name="name"/>
+      </div>
     </fieldset>
     <fieldset class="field">
-      <label for="contact-email">
-        <?php _e('E-mail'); ?>
-      </label>
-      <input class="required" type="email" id="contact-email" name="email"/>
+      <div class="field-inner">
+        <label for="contact-email">
+          <?php _e('E-mail'); ?>
+        </label>
+        <input class="required" type="email" id="contact-email" name="email"/>
+      </div>
     </fieldset>
     <fieldset class="field wide">
-      <label for="contact-message">
-        <?php _e('Message'); ?>
-      </label>
-      <textarea id="contact-message" name="message"></textarea>
+      <div class="field-inner">
+        <label for="contact-message">
+          <?php _e('Message'); ?>
+        </label>
+        <textarea id="contact-message" name="message"></textarea>
+      </div>
     </fieldset>
-  </fieldset>
+  </div>
   <input class="btn btn-small btn-success" type="submit" value="<?php _e('Send'); ?>" />
 </form>
