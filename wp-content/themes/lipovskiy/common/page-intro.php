c5b2d38dc6a0984a16ff9191836cc263c6c9d21f
Joe Blacky <gooddealthanks@gmail.com>
2016-09-09 15:35:23 +0300

    Unified page-intro. Fixed list-style bug in post-content.

--- wp-content/themes/lipovskiy/common/page-intro.php
+++ wp-content/themes/lipovskiy/common/page-intro.php
@@ -3,6 +3,6 @@
 	style="background-image: url('<?php echo get_the_post_thumbnail_url($post->ID); ?>"
 >
   <div class="description">
-    <?php the_title( '<h1 class="post-title">', '</h1>' ); ?>
+    <?php the_title( '<h1 class="post-title page-title">', '</h1>' ); ?>
   </div>
 </div>
